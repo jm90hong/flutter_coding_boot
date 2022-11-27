b5a0529e001eae1e02b6f4d41ff447bb42afc6f5
홍정민(GitMaster) <jm90.hong@gmail.com>
2022-11-28 08:17:34 +0900

    f

---
+++ src/main/java/com/my/flutter_coding_boot/FlutterCodingBootApplication.java
@@ -0,0 +1,13 @@
+package com.my.flutter_coding_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlutterCodingBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlutterCodingBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/flutter_coding_boot/FlutterCodingBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.flutter_coding_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlutterCodingBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
